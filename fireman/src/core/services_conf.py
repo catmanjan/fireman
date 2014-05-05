9eedf3bf223aa2c740e3d94aab44a90cd5c3c407
Matthew Whittaker <u5011539@anu.edu.au>
2014-05-05 15:15:59 +1000

    Added a new module - basically empty.

---
+++ fireman/src/core/services_conf.py
@@ -0,0 +1,17 @@
+"""
+Handles the interface to the service configuration files.
+This interface needs defining - so does the service configuration file
+format.
+
+This module will be used to query and update services and their rules.
+It is intended to abstract away how these are stored on the filesystem.
+
+STATE:PRE-SKELETON
+TODO:everything
+CONSIDERATIONS:
+  how should it look?
+"""
+
+# import master_conf - find the service config files from the master_conf
+
+pass
