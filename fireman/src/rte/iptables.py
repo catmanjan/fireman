d572a30561dd5034182b71cda05c66363f7ac41d
Oliver O'Neill <u5181490@anu.edu.au>
2014-05-08 16:09:22 +1000

    added deleting, finding and filtering rules. added objtodict module to convert objects into dictionaries

--- fireman/src/rte/iptables.py
+++ fireman/src/rte/iptables.py
@@ -1,23 +1,85 @@
 import subprocess
-import os
+import objtodict
+from collections import OrderedDict
 
-def execute(rules):
-    """ Takes an xml string following iptables-xml specification and adds the
-        rules to iptables
+class Iptables:
+    """ An Iptables which holds iptables' rule model
+        and deals with the interface between fireman and iptables
+    """
+    
+    def __init__(self, rule):
+        """ Make a new Iptables from a Rule object.
+            (Iptables, Rule) -> None
+        """
+        # add fields for the Iptables class using passed rule
+
+    def add_rule(self):
+        """ Add Iptables rule to iptables
+            (Iptables) -> None
+        """
+        # not implemented yet
+        raise NotImplementedError, "Adding rules is not yet available"
+        return
+
+        # will call iptables to add rules
+        try:
+            iptables_list = subprocess.check_call(["iptables", "-A", "INPUT"])
+        except subprocess.CalledProcessError:
+            print "error"
+
+    def delete_rule(self):
+        """ Delete the current rule
+            (Iptables) -> None
+        """
+        # delete rule using id
+        delete_rule(self.rule_id)
+
+def delete_rule(rule_id):
+    """ Delete an iptables rule using the rule_id stored in the comments of the rule
         (str) -> None
     """
-    # temporary file saved to hold xml
-    filename = "my-iptables.xml"
-    # xsltproc needs a file to read
-    text_file = open(filename, "w")
-    # save the xml string to a file
-    text_file.write(rules)
-    text_file.close()
-    # use iptables.xslt to transform the xml file to iptables commands
-    xsltproc = subprocess.Popen(["xsltproc", "iptables.xslt",
-                                filename], stdout=subprocess.PIPE)
-    # execute iptables rules using iptables-restore
-    subprocess.call(["iptables-restore"], stdin=xsltproc.stdout)
-    xsltproc.wait()
-    # remove the file
-    os.remove(filename)
+    # search for the rule using id
+    rules = find(rule_id)
+    # the first column stores the rule number - filter by this
+    indexes = filter(rules, 1)
+    # for each rule with that id
+    for index in indexes:
+        try:
+            # delete that rule
+            iptables_list = subprocess.check_call(["iptables", "-D", "INPUT", index])
+        except subprocess.CalledProcessError:
+            print "Error when deleting rule: " + iptables_list
+
+def find(rule_id):
+    """ Find an iptables rule using the rule_id stored in the comments of the rule
+        (str) -> str
+    """
+    rules = ""
+    # list the rules
+    iptables_list = subprocess.Popen(["iptables", "-L", "--line-numbers"], stdout=subprocess.PIPE)
+    try:
+        # grep to find the rule, which is stored in the comments
+        rules = subprocess.check_output(["grep", "\/\* " + rule_id + " \*\/"], stdin=iptables_list.stdout, stderr=subprocess.STDOUT)
+        iptables_list.wait()
+    except subprocess.CalledProcessError:
+        print "Error when finding rule: " + rules
+    # split the output into a list
+    return rules.splitlines()
+
+def filter(rules, column_index):
+    """ Filter rules by specified column index
+        ([str], int) -> [str]
+    """
+    fields = []
+    # filter the list of rules
+    for rule in rules:
+        rules_echo = subprocess.Popen(["echo", rule], stdout=subprocess.PIPE)
+        try:
+            # get the specified column using awk
+            field = subprocess.check_output(["awk", "{print $" + str(column_index) + "}"], stdin=rules_echo.stdout, stderr=subprocess.STDOUT)
+            rules_echo.wait()
+            # remove unnecessary whitespace
+            fields.append(field.strip())
+        except subprocess.CalledProcessError:
+            print "Error when filtering rules: " + field
+    return fields
--- fireman/src/ruletranslator.py
+++ fireman/src/ruletranslator.py
@@ -1,25 +1,37 @@
 from rte import iptables
-import dicttoxml, json
+from rte.iptables import Iptables
+import xmltodict, json
+import objtodict
 
-def execute(self, rules, format):
-    """ Execute the set of rules using the specified program
+def add_rule(rule, firewall_type):
+    """ Add the rules using the specified firewall
         (Rule, str) -> None
     """
-    if format == "iptables":
-        # convert to xml for iptables to translate
-        iptables.execute(translate("xml"))
+    if firewall_type == "iptables":
+        # create specific Iptables object
+        rule = Iptables(rule)
+        # add rule to iptables
+        rule.add_rule()
     else:
-        raise ValueError("Error: " + format + " is not yet supported")
+        raise ValueError("Error: " + firewall_type + " is not yet supported")
 
-def translate(self, rules format):
+def delete_rule(rule_id, program):
+    """ Delete the rule from the specified firewall, using the rule_id saved
+        in the comments of the firewall rule
+        (Rule, str) -> None
+    """
+    if firewall_type == "iptables":
+        iptables.delete_rule(rule_id)
+    else:
+        raise ValueError("Error: " + firewall_type + " is not yet supported")
+
+def translate(rule, format):
     """ Translate the set of rules into a specified format
         (Rule, str) -> None
     """
     if format == "json":
-        return json.dumps(rules.__dict__)
+        return json.dumps(objtodict.todict(rule))
     elif format == "xml":
-        return dicttoxml.parse(rules.__dict__)
+        return xmltodict.unparse(objtodict.todict(rule))
     else:
         raise ValueError("Error: " + format + " is not yet supported")
-
-
