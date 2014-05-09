1ad0e42718af3cc855e9a5f1ee962ba156e73179
Oliver O'Neill <u5181490@anu.edu.au>
2014-05-09 10:26:31 +1000

    added simple code for adding rules with target and id

--- fireman/src/rte/iptables.py
+++ fireman/src/rte/iptables.py
@@ -1,29 +1,51 @@
+""" Interfaces with iptables to add, delete and search for rules using
+    the Rule model and unique identifiers stored in comments
+"""
 import subprocess
 import objtodict
-from collections import OrderedDict
+
 
 class Iptables:
     """ An Iptables which holds iptables' rule model
         and deals with the interface between fireman and iptables
     """
-    
+
     def __init__(self, rule):
         """ Make a new Iptables from a Rule object.
             (Iptables, Rule) -> None
         """
-        # add fields for the Iptables class using passed rule
+        # the target of the rule ie. what to do if the packet matches
+        self.target = None
+        if rule.target == 0:
+            self.target = "ACCEPT"
+        elif rule.target == 1:
+            self.target = "DROP"
+
+        # matching criteria
+        self.conditions = rule.conditions
+        # where to add this rule in iptables - ignored if None
+        self.rule_number = rule.rule_number
+        # unique identifier
+        self.rule_id = rule.id
 
     def add_rule(self):
         """ Add Iptables rule to iptables
             (Iptables) -> None
         """
-        # not implemented yet
-        raise NotImplementedError, "Adding rules is not yet available"
-        return
+        args = ["iptables"]
+        if self.rule_number is None:
+            args.extend(["-A", "INPUT"])
+        else:
+            args.extend(["-I", "INPUT", str(self.rule_number)])
+        if self.target is not None:
+            args.extend(["-j", self.target])
+
+        args.extend(["-m", "comment", "--comment", self.rule_id])
+        #TODO: add conditions
 
         # will call iptables to add rules
         try:
-            iptables_list = subprocess.check_call(["iptables", "-A", "INPUT"])
+            iptables_list = subprocess.check_call(args)
         except subprocess.CalledProcessError:
             print "error"
 
@@ -34,8 +56,10 @@ class Iptables:
         # delete rule using id
         delete_rule(self.rule_id)
 
+
 def delete_rule(rule_id):
-    """ Delete an iptables rule using the rule_id stored in the comments of the rule
+    """ Delete an iptables rule using the rule_id stored in the
+        comments of the rule
         (str) -> None
     """
     # search for the rule using id
@@ -46,25 +70,34 @@ def delete_rule(rule_id):
     for index in indexes:
         try:
             # delete that rule
-            iptables_list = subprocess.check_call(["iptables", "-D", "INPUT", index])
+            iptables_list = subprocess.check_call(["iptables", "-D",
+                                                   "INPUT", index])
         except subprocess.CalledProcessError:
             print "Error when deleting rule: " + iptables_list
 
+
 def find(rule_id):
-    """ Find an iptables rule using the rule_id stored in the comments of the rule
+    """ Find an iptables rule using the rule_id stored in the comments
+        of the rule
         (str) -> str
     """
     rules = ""
     # list the rules
-    iptables_list = subprocess.Popen(["iptables", "-L", "--line-numbers"], stdout=subprocess.PIPE)
+    iptables_list = subprocess.Popen(["iptables", "-L",
+                                      "--line-numbers"],
+                                     stdout=subprocess.PIPE)
     try:
         # grep to find the rule, which is stored in the comments
-        rules = subprocess.check_output(["grep", "\/\* " + rule_id + " \*\/"], stdin=iptables_list.stdout, stderr=subprocess.STDOUT)
+        rules = subprocess.check_output(
+            ["grep", "\/\* " + rule_id + " \*\/"],
+            stdin=iptables_list.stdout,
+            stderr=subprocess.STDOUT)
         iptables_list.wait()
     except subprocess.CalledProcessError:
         print "Error when finding rule: " + rules
     # split the output into a list
     return rules.splitlines()
+
 
 def filter(rules, column_index):
     """ Filter rules by specified column index
@@ -73,10 +106,14 @@ def filter(rules, column_index):
     fields = []
     # filter the list of rules
     for rule in rules:
-        rules_echo = subprocess.Popen(["echo", rule], stdout=subprocess.PIPE)
+        rules_echo = subprocess.Popen(["echo", rule],
+                                      stdout=subprocess.PIPE)
         try:
             # get the specified column using awk
-            field = subprocess.check_output(["awk", "{print $" + str(column_index) + "}"], stdin=rules_echo.stdout, stderr=subprocess.STDOUT)
+            field = subprocess.check_output(["awk", "{print $" +
+                                            str(column_index) + "}"],
+                                            stdin=rules_echo.stdout,
+                                            stderr=subprocess.STDOUT)
             rules_echo.wait()
             # remove unnecessary whitespace
             fields.append(field.strip())
--- fireman/src/ruletranslator.py
+++ fireman/src/ruletranslator.py
@@ -1,29 +1,53 @@
+""" An interface for translating and executing rules using specific
+    formats and firewall services
+"""
 from rte import iptables
 from rte.iptables import Iptables
-import xmltodict, json
+import xmltodict
+import json
 import objtodict
 
-def add_rule(rule, firewall_type):
+
+def add_rule(rule, service_name):
     """ Add the rules using the specified firewall
         (Rule, str) -> None
     """
-    if firewall_type == "iptables":
+    if service_name == "iptables":
         # create specific Iptables object
         rule = Iptables(rule)
         # add rule to iptables
         rule.add_rule()
     else:
-        raise ValueError("Error: " + firewall_type + " is not yet supported")
+        raise ValueError("Error: " + service_name +
+                         " is not yet supported")
 
-def delete_rule(rule_id, program):
-    """ Delete the rule from the specified firewall, using the rule_id saved
-        in the comments of the firewall rule
-        (Rule, str) -> None
+
+def delete_rule_using_id(rule_id, service_name):
+    """ Delete the rule from the specified firewall, using the
+        rule_id saved in the comments of the firewall rule
+        (str, str) -> None
     """
-    if firewall_type == "iptables":
+    if service_name == "iptables":
         iptables.delete_rule(rule_id)
     else:
-        raise ValueError("Error: " + firewall_type + " is not yet supported")
+        raise ValueError("Error: " + service_name +
+                         " is not yet supported")
+
+
+def delete_rule(rule, service_name):
+    """ Delete the specified Rule object from the specified firewall
+        service
+        (Rule, str) -> None
+    """
+    if service_name == "iptables":
+        # create specific Iptables object
+        rule = Iptables(rule)
+        # add rule to iptables
+        rule.delete_rule()
+    else:
+        raise ValueError("Error: " + service_name +
+                         " is not yet supported")
+
 
 def translate(rule, format):
     """ Translate the set of rules into a specified format
