470e7d3790e83e0420f0ed3beccb3cce05092e26
Jan Martin <u5013925@anu.edu.au>
2014-09-02 19:28:16 +1000

    test

--- src/tests/ruletranslator.py
+++ src/tests/ruletranslator.py
@@ -11,19 +11,25 @@ class RuleTranslatorTests(unittest.TestCase):
         # Do a test for rules with None for an ID (edgecase?)
         self.noneRule = Rule(None, 0, 0, [])
 
+        # TODO rule with alternate values for action and chain
+        # TODO rule with alternate values for conditions
+
     def test_add_rule(self):
+        # TODO
         self.assertEqual(True, True)
 
     def test_delete_rule_using_id(self):
+        # TODO
         self.assertEqual(True, True)
 
     def test_delete_rule(self):
+        # TODO
         self.assertEqual(True, True)
 
     def test_translate(self):
         blankRuleJson = "{\"action\": 0, \"conditions\": [], \"id\": \"blank\", \"chain\": 0}"
         noneRuleJson = "{\"action\": 0, \"conditions\": [], \"chain\": 0}"
-        
+
         self.assertEqual(rte.translate(self.blankRule, "json"), blankRuleJson)
         self.assertEqual(rte.translate(self.noneRule, "json"), noneRuleJson)
 
